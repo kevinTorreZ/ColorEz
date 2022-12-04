ac4ea8be5beeda2d5aa36e0850c384f49177bc34
kevinTorreZ <61859021+kevinTorreZ@users.noreply.github.com>
2022-12-04 20:46:58 -0300

    a
    a

--- ColorEz/settings.py
+++ ColorEz/settings.py
@@ -136,6 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL='/media/'
+STATIC_ROOT = '/home/coloreze/static'
+MEDIA_ROOT = '/home/coloreze/media'
 AUTH_USER_MODEL = 'Principal.Usuario'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
