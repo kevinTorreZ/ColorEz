5702bcb748173d0dd4adc0f60b2c06475849845f
kevinTorreZ <61859021+kevinTorreZ@users.noreply.github.com>
2022-12-06 23:26:18 -0300

    a
    a

--- Principal/static/js/inicio.js
+++ Principal/static/js/inicio.js
@@ -1,16 +1,4 @@
-function GetPaleta(){
-    var highlightedItems = document.querySelectorAll('.colorPalette')
-    if (highlightedItems.length != 0) {
-        highlightedItems.forEach(function (element) {
-            element.remove();
-        })
-    }
-    color = document.getElementById('colorPalette').value
-    var arrayPaleta = chroma.scale([color,'white']).colors(10)
-    for (var color of arrayPaleta) {
-        document.getElementById('paletteColor').innerHTML += "<div class='colorPalette' style='background-color:"+color+";width:180px;height:300px;color:white;'>"+color+"</div>"
-    }
-}
+
 
 document.getElementById("html").addEventListener("keydown", function (e) {
 
@@ -45,7 +33,7 @@ function generatePalette() {
         })
     }
     for (i = 0; i < 5; i++) {
-        document.getElementById('paleta_colores').innerHTML += '<div class="color" style="background-color:' + randomHexColor() + ';width:300px;height:300px;margin:10px">' + randomHexColor() + '</div>'
+        document.getElementById('paleta_colores').innerHTML += '<div class="color" style="background-color:' + randomHexColor() + ';width:170px;height:300px;margin:10px">' + randomHexColor() + '</div>'
     }
 }
 window.onload = function(){
@@ -62,9 +50,8 @@ function Mixcolor() {
     color1 = document.getElementById('Mixcolor1').value
     color2 = document.getElementById('Mixcolor2').value
     var arrayColors = chroma.scale([color1, color2]).mode('lch').colors(10)
-    var cola = chroma.scale([color1,'white']).colors(10)
     for (var color of arrayColors) {
-        document.getElementById('MixColors').innerHTML += "<div class='colorofMix' style='background-color:"+color+";width:180px;height:300px;color:white;'>"+color+"</div>"
+        document.getElementById('MixColors').innerHTML += "<div class='colorofMix' style='background-color:"+color+";width:170px;height:300px;color:white;'>"+color+"</div>"
     }
 }
 
