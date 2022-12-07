4e7696b7c88ba135257e26b2cdfdda12ba4545bc
kevinTorreZ <61859021+kevinTorreZ@users.noreply.github.com>
2022-12-07 01:03:42 -0300

    a
    a

--- Principal/admin.py
+++ Principal/admin.py
@@ -1,9 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
-from Principal.models import Usuario
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import UserAdminCreationForm, UserAdminChangeForm
+
+usuario = get_user_model()
 
 admin.site.unregister(Group)
-class UsuarioAdmin(admin.ModelAdmin):
-   list_display = ['id', 'username','password','email','photo','activo','admin','staff','last_login']
 
-admin.site.register(Usuario, UsuarioAdmin)
+class UserAdmin(BaseUserAdmin):
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
+
+    list_display = ['email', 'username','password','admin', 'activo','photo']
+    list_filter = ['admin']
+    fieldsets = (
+        (None, {'fields': ('email', 'password','username')}),
+        ('Personal info', {'fields': ()}),
+        ('Permissions', {'fields': ('admin','activo','photo')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username','password','admin', 'activo','photo')}
+        ),
+    )
+    search_fields = ['username']
+    ordering = ['username']
+    filter_horizontal = ()
+admin.site.register(usuario, UserAdmin)
--- Principal/forms.py
+++ Principal/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from Principal.models import Usuario
 from django.shortcuts import render,redirect
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 import string
 class RegisterForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Ingrese su Contraseña'}), label='')
@@ -68,3 +69,35 @@ class LoginForm(forms.Form):
         username = cleaned_data.get('username')
         password = cleaned_data.get('password')
         return authenticate(username=username, password=password)
+
+class UserAdminCreationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = Usuario
+        fields = ['username','email']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_2 = cleaned_data.get("password_2")
+        if password is not None and password != password_2:
+            self.add_error("password_2", "Your passwords must match")
+        return cleaned_data
+
+    def save(self, commit=True):
+        Usuario = super().save(commit=False)
+        Usuario.set_password(self.cleaned_data["password"])
+        if commit:
+            Usuario.save()
+        return Usuario                                                                                                                                                                              
+class UserAdminChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = Usuario
+        fields = ['username','email', 'password', 'activo', 'admin', 'photo']
+
+    def clean_password(self):
+        return self.initial["password"]
