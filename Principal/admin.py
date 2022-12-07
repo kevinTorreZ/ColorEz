78348a9fa86277cd6ac79b6665be0f9983d4ff7a
kevinTorreZ <61859021+kevinTorreZ@users.noreply.github.com>
2022-12-07 00:54:58 -0300

    a
    a

--- Principal/admin.py
+++ Principal/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from Principal.models import Usuario
 
+admin.site.unregister(Group)
+class UsuarioAdmin(admin.ModelAdmin):
+   list_display = ['id', 'username','password','email','photo','activo','admin','staff','last_login']
 
+admin.site.register(Usuario, UsuarioAdmin)
