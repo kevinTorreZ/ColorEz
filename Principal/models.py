9184d67e3dd045acb6c13792db55815eb07f44d8
kevinTorreZ <61859021+kevinTorreZ@users.noreply.github.com>
2022-12-08 23:10:46 -0300

    a
    a

--- Principal/models.py
+++ Principal/models.py
@@ -96,7 +96,7 @@ class Proyecto(models.Model):
     Titulo = models.CharField(max_length=25)
     Descripcion = models.TextField()
     Fecha_creacion = models.DateField(verbose_name='',)
-    photo = models.ImageField(upload_to = 'home/coloreze/ColorEz/Principal/static/img',verbose_name='',default='/home/coloreze/ColorEz/Principal/static/img/default_image_project.png')
+    photo = models.ImageField(upload_to = 'home/coloreze/public_html/media/png',verbose_name='',default='/home/coloreze/public_html/media/default_image_project.png')
     Usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE,verbose_name='',)
     def __str__(self):
        return self.Titulo
