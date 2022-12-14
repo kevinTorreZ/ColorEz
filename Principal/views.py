3eae76b8123741ca454d58fb909ea77ad2f97d04
kevinTorreZ <61859021+kevinTorreZ@users.noreply.github.com>
2022-12-14 05:29:45 -0300

    a
    a

--- Principal/views.py
+++ Principal/views.py
@@ -332,12 +332,12 @@ def Perfil(request):
 
 
 def handler404(request, exception, template_name="404.html"):
-    response = render_to_response(template_name)
+    response = render(request,template_name)
     response.status_code = 404
     return response
 
 
 def handler500(request,template_name="500.html"):
-    response = render_to_response(template_name)
+    response = render(request,template_name)
     response.status_code = 500
     return response
